df4e3d33e04256200d27dc8085a24d12817a0d83
Rachel Brandt <rachel.brandt@gmail.com>
2017-03-01 12:35:46 -0500

    add album.js for click event on album page between three albums

--- scripts/album.js
+++ scripts/album.js
@@ -29,6 +29,21 @@ var albumMarconi = {
         { title: 'Wrong phone number', duration: '2:15'}
     ]
 };
+//assignment third example album
+var albumHotFuss = {
+    title: 'Hot Fuss',
+    artist: 'The Killers',
+    label: 'NA',
+    year: '2003',
+    albumArtUrl: 'assets/images/album_covers/21.png',
+    songs: [
+        { title: 'Hello Song?', duration: '4:01' },
+        { title: 'Song, ring, ring', duration: '4:41' },
+        { title: 'This song sits in your pocket', duration: '4:44'},
+        { title: 'Can you see me now?', duration: '4:14' },
+        { title: 'Correct phone number', duration: '4:15'}
+    ]
+};
 
 var createSongRow = function(songNumber, songName, songLength) {
     var template =
@@ -42,24 +57,23 @@ var createSongRow = function(songNumber, songName, songLength) {
     return template;
 };
 
-var setCurrentAlbum = function(album) {
-     // #1
      var albumTitle = document.getElementsByClassName('album-view-title')[0];
      var albumArtist = document.getElementsByClassName('album-view-artist')[0];
      var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
      var albumImage = document.getElementsByClassName('album-cover-art')[0];
      var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
- 
-     // #2
+
+var setCurrentAlbum = function(album) {
+  // assign values to each part of the album (text, images)
      albumTitle.firstChild.nodeValue = album.title;
      albumArtist.firstChild.nodeValue = album.artist;
      albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
      albumImage.setAttribute('src', album.albumArtUrl);
  
-     // #3
+     // clear occntents of album song list container
      albumSongList.innerHTML = '';
  
-     // #4
+     // build list of songs from album JavaScript object
      for (var i = 0; i < album.songs.length; i++) {
          albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
      }
@@ -67,4 +81,14 @@ var setCurrentAlbum = function(album) {
  
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
+     
+     var albums = [albumPicasso, albumMarconi, albumHotFuss];
+     var index = 1;
+     albumImage.addEventListener("click", function(event) {
+         setCurrentAlbum(albums[index]);
+         index++;
+         if (index == albums.length) {
+             index = 0;
+         }
+     });
  };
