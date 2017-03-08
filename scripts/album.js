98258570a45b2d3d55ae9850c3701ef2aecbb35b
Rachel Brandt <rachel.brandt@gmail.com>
2017-03-08 16:50:55 -0500

    assignment

--- scripts/album.js
+++ scripts/album.js
@@ -80,6 +80,8 @@ var setCurrentAlbum = function(album) {
 
 var findParentByClassName = function(element, targetClass) {
     if (element) {
+        
+        
         var currentParent = element.parentElement;
         while (currentParent.className !== targetClass && currentParent.className !== null) {
             currentParent = currentParent.parentElement;
